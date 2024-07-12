2c63e2b1f384dfe6f4ffbcd209b415f889e99c26
ekko0102 <63711968+ekko0102@users.noreply.github.com>
2024-07-12 12:09:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,24 +3,26 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
 import os
+import openai
 import time
 import traceback
-from openai import OpenAI
 
 app = Flask(__name__)
 static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
 
-# 初始化LineBotApi和WebhookHandler
+# Channel Access Token
 line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
+# Channel Secret
 handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
+# OPENAI API Key初始化設定
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
-# 初始化OpenAI API Key
-ASSISTANT_ID = "asst_H4JiVadUvQzVI77CrgsdOk62"
+ASSISTANT_ID = os.getenv('OPENAI_MODEL_ID')
 
 def GPT_response(text):
-    client = OpenAI()
+    client = openai.OpenAI()
     
-    # 创建一个新的对话线程
+    # Create a thread with a message
     thread = client.beta.threads.create(
         messages=[
             {
@@ -30,49 +32,44 @@ def GPT_response(text):
         ]
     )
     
-    # 提交线程并创建新的运行
+    # Submit the thread to the assistant (as a new run)
     run = client.beta.threads.runs.create(thread_id=thread.id, assistant_id=ASSISTANT_ID)
     
-    # 等待运行完成
+    # Wait for run to complete
     while run.status != "completed":
         run = client.beta.threads.runs.retrieve(thread_id=thread.id, run_id=run.id)
-        print(f"🏃 Run Status: {run.status}")
         time.sleep(1)
     
-    # 获取最新的消息
+    # Get the latest message from the thread
     message_response = client.beta.threads.messages.list(thread_id=thread.id)
     messages = message_response.data
-    latest_message = messages[0]
-    return latest_message['content'][0]['text']['value']
 
-# 监控所有来自 /callback 的 POST 请求
+    # Print the latest message
+    latest_message = messages[0]
+    return latest_message.content[0].text.value
+
 @app.route("/callback", methods=['POST'])
 def callback():
-    # 获取X-Line-Signature头值
     signature = request.headers['X-Line-Signature']
-    
-    # 获取请求体内容
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
     
-    # 处理Webhook主体
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
+    
     return 'OK'
 
-# 处理消息事件
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
     try:
         GPT_answer = GPT_response(msg)
-        print(GPT_answer)
         line_bot_api.reply_message(event.reply_token, TextSendMessage(GPT_answer))
     except:
         print(traceback.format_exc())
-        line_bot_api.reply_message(event.reply_token, TextSendMessage('你所使用的OPENAI API key额度可能已經超過，請於後台Log內確認錯誤訊息'))
+        line_bot_api.reply_message(event.reply_token, TextSendMessage('你所使用的OPENAI API key額度可能已經超過，請於後台Log內確認錯誤訊息'))
 
 @handler.add(PostbackEvent)
 def handle_message(event):
