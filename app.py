1029244503495cdbc447059d905e8dbf34c9026a
ekko0102 <63711968+ekko0102@users.noreply.github.com>
2024-10-29 23:27:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -113,7 +113,7 @@ def callback():
     except InvalidSignatureError:
         abort(400)
     
-    return 'OK'
+    return 'OK',200
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
